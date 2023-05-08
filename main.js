888298d4c0221f66f2e76eaa3a7bcc0c99574de3
Geovann Chandra <103976069+gechdr@users.noreply.github.com>
2023-05-08 14:56:25 +0700

    update

--- main.js
+++ main.js
@@ -17,10 +17,14 @@ const texture = backgroundLoader.load("textures/Sky1.jpg");
 const scene = new THREE.Scene();
 scene.background = texture;
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+// scene.add(ambientLight);
 
-const light = new THREE.PointLight(0xffffff, 2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+const light = new THREE.PointLight(0xffffff, 10);
+// (,ketinggian,)
 light.position.set(0, 0, 0);
 
 light.castShadow = true;
@@ -30,22 +34,40 @@ light.shadow.camera.near = 1;
 light.shadow.camera.far = 1000;
 scene.add(light);
 
-light.position.set(10, 10, 10);
+// light.position.set(10, 10, 10);
 
-light.castShadow = true;
-light.shadow.mapSize.width = width;
-light.shadow.mapSize.height = height;
-light.shadow.camera.near = 1;
-light.shadow.camera.far = 1000;
-scene.add(light);
+// light.castShadow = true;
+// light.shadow.mapSize.width = width;
+// light.shadow.mapSize.height = height;
+// light.shadow.camera.near = 1;
+// light.shadow.camera.far = 1000;
+// scene.add(light);
 
 // camera
-const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
 camera.position.set(50, 20, 150);
 camera.lookAt(0, 0, 0);
 
 const loader = new GLTFLoader();
 
+let land;
+loader.load(
+	// resource URL
+	"models/land.glb",
+	function (gltf) {
+		land = gltf.scene;
+		// land.castShadow = true;
+		land.position.x = 0;
+		land.scale.x = 20;
+		land.scale.y = 20;
+		land.scale.z = 20;
+		console.log(land);
+
+		scene.add(land);
+	}
+);
+
+let mixerPlane;
 let plane;
 loader.load(
 	// resource URL
@@ -58,29 +80,37 @@ loader.load(
 		plane.scale.y = 8;
 		plane.scale.z = 8;
 
+		mixerPlane = new THREE.AnimationMixer(plane);
+		const clips = gltf.animations;
+		console.log("plane");
+		console.log(clips);
+		const clip = THREE.AnimationClip.findByName(clips, "Animation");
+		const action = mixerPlane.clipAction(clip);
+		action.play();
+
 		// console.log(plane);
 		scene.add(plane);
 	}
 );
 
-let ballon;
-loader.load(
-	// resource URL
-	"models/zeppelin_air_balloon.glb",
-	function (gltf) {
-		ballon = gltf.scene;
-		ballon.castShadow = true;
-		ballon.position.x = 70;
-		ballon.position.z = -90;
-		ballon.scale.x = 1;
-		ballon.scale.y = 1;
-		ballon.scale.z = 1;
-		ballon.rotation.y = 30;
+// let ballon;
+// loader.load(
+// 	// resource URL
+// 	"models/zeppelin_air_balloon.glb",
+// 	function (gltf) {
+// 		ballon = gltf.scene;
+// 		ballon.castShadow = true;
+// 		ballon.position.x = 70;
+// 		ballon.position.z = -90;
+// 		ballon.scale.x = 1;
+// 		ballon.scale.y = 1;
+// 		ballon.scale.z = 1;
+// 		ballon.rotation.y = 30;
 
-		// console.log(plane);
-		scene.add(ballon);
-	}
-);
+// 		// console.log(plane);
+// 		scene.add(ballon);
+// 	}
+// );
 
 let mixerHeli;
 let heli;
@@ -107,30 +137,30 @@ loader.load(
 	}
 );
 
-let mixerRocket;
-let rocket;
-loader.load(
-	// resource URL
-	"models/rocket_ship.glb",
-	function (gltf) {
-		rocket = gltf.scene;
-		rocket.castShadow = true;
-		rocket.position.x = 0;
-		rocket.position.z = 40;
-		rocket.scale.x = 0.1;
-		rocket.scale.y = 0.1;
-		rocket.scale.z = 0.1;
+// let mixerRocket;
+// let rocket;
+// loader.load(
+// 	// resource URL
+// 	"models/rocket_ship.glb",
+// 	function (gltf) {
+// 		rocket = gltf.scene;
+// 		rocket.castShadow = true;
+// 		rocket.position.x = 0;
+// 		rocket.position.z = 40;
+// 		rocket.scale.x = 0.1;
+// 		rocket.scale.y = 0.1;
+// 		rocket.scale.z = 0.1;
 
-		mixerRocket = new THREE.AnimationMixer(rocket);
-		const clips = gltf.animations;
-		console.log(clips);
-		const clip = THREE.AnimationClip.findByName(clips, "Take 001");
-		const action = mixerRocket.clipAction(clip);
-		action.play();
-		// console.log(rocket);
-		scene.add(rocket);
-	}
-);
+// 		mixerRocket = new THREE.AnimationMixer(rocket);
+// 		const clips = gltf.animations;
+// 		// console.log(clips);
+// 		const clip = THREE.AnimationClip.findByName(clips, "Take 001");
+// 		const action = mixerRocket.clipAction(clip);
+// 		action.play();
+// 		// console.log(rocket);
+// 		scene.add(rocket);
+// 	}
+// );
 
 let mixerDrone;
 let drone;
@@ -145,16 +175,16 @@ loader.load(
 		drone.scale.x = 0.1;
 		drone.scale.y = 0.1;
 		drone.scale.z = 0.1;
-		console.log(drone);
-		
-		// mixerDrone = new THREE.AnimationMixer(drone);
-		// const clips = gltf.animations;
-		// console.log(clips);
-		// const clip = THREE.AnimationClip.findByName(clips, "Sphere");
-		// const action = mixerDrone.clipAction(clip);
-		// action.play();
+		// console.log("drone");f
 
-		console.log(drone);
+		mixerDrone = new THREE.AnimationMixer(drone);
+		const clips = gltf.animations;
+		// console.log(clips);
+		const clip = THREE.AnimationClip.findByName(clips, "Take 01");
+		const action = mixerDrone.clipAction(clip);
+		action.play();
+
+		// console.log(drone);
 		scene.add(drone);
 	}
 );
@@ -172,7 +202,7 @@ loader.load(
 		hot_air.scale.x = 1;
 		hot_air.scale.y = 1;
 		hot_air.scale.z = 1;
-		
+
 		// console.log(hot_air);
 		scene.add(hot_air);
 	}
@@ -200,55 +230,56 @@ window.addEventListener("resize", () => {
 });
 
 // Controls
-// let controls1 = new FlyControls(camera, renderer.domElement);
-// controls1.movementSpeed = 10;
-// controls1.rollSpeed = 0.05;
-// controls1.autoForward = false;
-// controls1.dragToLook = false;
+let controls1 = new FlyControls(camera, renderer.domElement);
+controls1.movementSpeed = 10;
+controls1.rollSpeed = 0.05;
+controls1.autoForward = false;
+controls1.dragToLook = false;
 
 let controls = new OrbitControls(camera, renderer.domElement);
 controls.autoRotate = false;
-controls.target = new THREE.Vector3(2,2,2);
+controls.target = new THREE.Vector3(2, 2, 2);
 
-controls.keys = {
-	LEFT: "KeyA",
-	UP: "KeyW",
-	RIGHT: "KeyD",
-	BOTTOM: "KeyS"
-};
-controls.listenToKeyEvents(window);
+// controls.keys = {
+// 	LEFT: "KeyA",
+// 	UP: "KeyW",
+// 	RIGHT: "KeyD",
+// 	BOTTOM: "KeyS"
+// };
+// controls.listenToKeyEvents(window);
 // controls.addEventListener(window);
 
 let turnPlane = 1;
 let turnHeli = 1;
-let turnBallon = 1;
+// let turnBallon = 1;
 let turnHotAir = 1;
 
 const clockHeli = new THREE.Clock();
 const clockRocket = new THREE.Clock();
 const clockDrone = new THREE.Clock();
+const clockPlane = new THREE.Clock();
 function animate() {
-	if (plane != undefined) {
-		// plane.position.x += 0.09;
-		// plane.position.y += 0.09;
+	// if (plane != undefined) {
+	// 	// plane.position.x += 0.09;
+	// 	// plane.position.y += 0.09;
 
-		if (turnPlane == 1) {
-			plane.position.z += 0.09;
-		} else {
-			plane.position.z -= 0.09;
-		}
+	// 	if (turnPlane == 1) {
+	// 		plane.position.z += 0.09;
+	// 	} else {
+	// 		plane.position.z -= 0.09;
+	// 	}
 
-		if (plane.position.z > 100) {
-			turnPlane = 0;
-			plane.rotation.y = 600;
-		} else if (plane.position.z < -100) {
-			turnPlane = 1;
-			plane.rotation.y = 0;
-		}
+	// 	if (plane.position.z > 100) {
+	// 		turnPlane = 0;
+	// 		plane.rotation.y = 600;
+	// 	} else if (plane.position.z < -100) {
+	// 		turnPlane = 1;
+	// 		plane.rotation.y = 0;
+	// 	}
 
-		// console.log(plane.rotation.z);
-		// camera.lookAt(shark.position)
-	}
+	// 	// console.log(plane.rotation.z);
+	// 	// camera.lookAt(shark.position)
+	// }
 
 	if (heli != undefined) {
 		// heli.position.x += 0.09;
@@ -271,51 +302,56 @@ function animate() {
 		// camera.lookAt(shark.position)
 	}
 
-	if (ballon != undefined) {
-		// heli.position.x += 0.09;
-		// heli.position.y += 0.09;
+	// if (ballon != undefined) {
+	// 	// heli.position.x += 0.09;
+	// 	// heli.position.y += 0.09;
 
-		// y 70 z -90
+	// 	// y 70 z -90
 
-		if (turnBallon == 1) {
-			ballon.position.y -= 0.5;
-			ballon.position.x -= 1;
-		} else {
-			ballon.position.y += 0.5;
-			ballon.position.x += 1;
-		}
+	// 	if (turnBallon == 1) {
+	// 		ballon.position.y -= 0.5;
+	// 		ballon.position.x -= 1;
+	// 	} else {
+	// 		ballon.position.y += 0.5;
+	// 		ballon.position.x += 1;
+	// 	}
 
-		if (ballon.position.y > 60) {
-			turnBallon = 1;
-			// ballon.rotation.x = 600;
-			// ballon.rotation.y = 0;
-			// ballon.rotation.z = 0;
-		} else if (ballon.position.y < -100) {
-			turnBallon = 0;
-			// ballon.rotation.x = 100;
-			// ballon.rotation.y = 200;
-			// ballon.rotation.z = 100;
-		}
+	// 	// console.log(ballon);
+	// 	// if (ballon.position.y > 60) {
+	// 	// 	turnBallon = 1;
+	// 	// 	// ballon.rotation.x = 600;
+	// 	// 	// ballon.rotation.y = 600;
+	// 	// 	// ballon.rotation.z = 0;
+	// 	// 	// ballon.rotation._y = 500;
+	// 	// } else if (ballon.position.y < -100) {
+	// 	// 	turnBallon = 0;
+	// 	// 	// ballon.rotation.x = 100;
+	// 	// 	// ballon.rotation.y = 200;
+	// 	// 	// ballon.rotation.z = 100;
+	// 	// }
 
-		// camera.lookAt(shark.position)
-	}
+	// 	// camera.lookAt(shark.position)
+	// }
 
-	if (rocket != undefined) {
-		if (rocket.position.y < 100) {
-			rocket.position.y += 0.5;
-			rocket.position.x -= 1;
-			// rocket.rotation.y += 0.01;
-			// rocket.rotation.z += 0.1;
-			// rocket.rotation.x += 0.1;
-		} else {
-			rocket.position.y = -50;
-			rocket.position.x = 100;
-			
-			// ballon/.rotation.y = 0;
-		}
+	// if (rocket != undefined) {
+	// 	if (rocket.position.y < 600) {
+	// 		rocket.position.y += 0.5;
+	// 		rocket.position.x -= 1;
 
-		// camera.lookAt(shark.position)
-	}
+	// 		// rocket.rotation.y += 0.01;
+	// 		// rocket.rotation.z += 0.1;
+	// 		// rocket.rotation.x += 0.1;
+	// 	} else {
+	// 		// rocket.position.y = -50;
+	// 		// rocket.position.x = 0;
+	// 		rocket.position.y = -300;
+	// 		rocket.position.x = 400;
+
+	// 		// ballon/.rotation.y = 0;
+	// 	}
+
+	// 	// camera.lookAt(shark.position)
+	// }
 
 	if (hot_air != undefined) {
 		// heli.position.x += 0.09;
@@ -341,18 +377,20 @@ function animate() {
 	if (mixerHeli) {
 		mixerHeli.update(clockHeli.getDelta());
 	}
-	if (mixerRocket) {
-		mixerRocket.update(clockRocket.getDelta());
-	}
+	// if (mixerRocket) {
+	// 	mixerRocket.update(clockRocket.getDelta());
+	// }
 	if (mixerDrone) {
 		mixerDrone.update(clockDrone.getDelta());
 	}
+	if (mixerPlane) {
+		mixerPlane.update(clockPlane.getDelta());
+	}
 
-	
 	// console.log(cube.position.x)
-	
+
 	//For FlyControls
-	// controls1.update(0.05);
+	controls1.update(0.05);
 	controls.update();
 
 	requestAnimationFrame(animate);
