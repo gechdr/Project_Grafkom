b118f6f8eb2659fdd835fe1c72b0ea3c4c12ee3c
DarrellFiko <darrellfikoalexander@gmail.com>
2023-05-14 16:28:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,16 +18,21 @@ const scene = new THREE.Scene();
 scene.background = texture;
 
 // Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xFFFFFFF);
 scene.add(ambientLight);
 
 // const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 // scene.add(directionalLight);
 
 // const light = new THREE.PointLight(0xffffff, 12);
-const light = new THREE.DirectionalLight(0xffffff, 12);
+const light = new THREE.PointLight(0xffffff, 8);
 // (,ketinggian,)
 light.position.set(0, 1000, 0);
+
+// light.intensity = 10;
+light.angle = 5;
+// light.penumbra = 0.3;
+light.shadow.focus = 5;
 
 // light.shadow.camera.left = -1000000
 // light.shadow.camera.right = 1000000
@@ -36,8 +41,8 @@ light.position.set(0, 1000, 0);
 light.castShadow = true;
 light.shadow.mapSize.width = width;
 light.shadow.mapSize.height = height;
-light.shadow.camera.near = 0.1;
-light.shadow.camera.far = 100000;
+light.shadow.camera.near = 0.001;
+light.shadow.camera.far = 10000;
 scene.add(light);
 
 // camera
@@ -76,14 +81,14 @@ loader.load(
 	"models/airport_building1.glb",
 	function (gltf) {
 		airport_building1 = gltf.scene;
-		airport_building1.castShadow = true;
+		// airport_building1.castShadow = true;
 		airport_building1.position.x = 110;
 		airport_building1.position.y = 0;
 		airport_building1.position.z = -138;
 		airport_building1.scale.x = 1000;
 		airport_building1.scale.y = 1000;
 		airport_building1.scale.z = 1000;
-
+		airport_building1.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		// console.log(plane);
 		scene.add(airport_building1);
 	}
@@ -95,7 +100,7 @@ loader.load(
 	"models/airport_building2.glb",
 	function (gltf) {
 		airport_building2 = gltf.scene;
-		airport_building2.castShadow = true;
+		// airport_building2.castShadow = true;
 		airport_building2.position.x = 130;
 		airport_building2.position.y = 0;
 		airport_building2.position.z = 55;
@@ -103,7 +108,7 @@ loader.load(
 		airport_building2.scale.y = 0.04;
 		airport_building2.scale.z = 0.04;
 		airport_building2.rotation.y = -300;
-
+		airport_building2.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		// console.log(plane);
 		scene.add(airport_building2);
 	}
@@ -114,7 +119,7 @@ loader.load(
 	"models/airport_building2.glb",
 	function (gltf) {
 		airport_building2 = gltf.scene;
-		airport_building2.castShadow = true;
+		// airport_building2.castShadow = true;
 		airport_building2.position.x = 130;
 		airport_building2.position.y = 0;
 		airport_building2.position.z = 135;
@@ -122,7 +127,7 @@ loader.load(
 		airport_building2.scale.y = 0.04;
 		airport_building2.scale.z = 0.04;
 		airport_building2.rotation.y = -300;
-
+		airport_building2.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		// console.log(plane);
 		scene.add(airport_building2);
 	}
@@ -134,6 +139,7 @@ loader.load(
 	"models/helipad.glb",
 	function (gltf) {
 		helipad = gltf.scene;
+		helipad.receiveShadow = true;
 		// helipad.castShadow = true;
 		helipad.position.x = -125;
 		helipad.position.y = 1;
@@ -154,14 +160,14 @@ loader.load(
 	"models/plane.glb",
 	function (gltf) {
 		plane = gltf.scene;
-		plane.castShadow = true;
+		// plane.castShadow = true;
 		plane.position.x = -15;
 		plane.position.y = 6;
 		plane.position.z = 0;
 		plane.scale.x = 3;
 		plane.scale.y = 3;
 		plane.scale.z = 3;
-
+		plane.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		mixerPlane = new THREE.AnimationMixer(plane);
 		const clips = gltf.animations;
 		// console.log("plane");
@@ -183,14 +189,14 @@ loader.load(
 	"models/heli.glb",
 	function (gltf) {
 		heli = gltf.scene;
-		heli.castShadow = true;
+		// heli.castShadow = true;
 		heli.position.x = -125;
 		heli.position.y = 5.3;
 		heli.position.z = -110;
 		heli.scale.x = 3;
 		heli.scale.y = 3;
 		heli.scale.z = 3;
-
+		heli.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		mixerHeli = new THREE.AnimationMixer(heli);
 		const clips = gltf.animations;
 		// console.log(clips);
@@ -214,7 +220,7 @@ loader.load(
 	"models/s9_mini_drone.glb",
 	function (gltf) {
 		drone = gltf.scene;
-		drone.castShadow = true;
+		// drone.castShadow = true;
 		drone.position.x = 0;
 		drone.position.z = 40;
 		drone.scale.x = 0.1;
@@ -240,7 +246,7 @@ loader.load(
 	"models/steampunk_hot_air_balloon.glb",
 	function (gltf) {
 		hot_air = gltf.scene;
-		hot_air.castShadow = true;
+		// hot_air.castShadow = true;
 		hot_air.position.x = -125;
 		hot_air.position.y = 0;
 		hot_air.position.z = 100;
@@ -248,6 +254,7 @@ loader.load(
 		hot_air.scale.y = 3;
 		hot_air.scale.z = 3;
 
+		hot_air.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		// hot_air.traverse( function( node ) { if ( node instanceof THREE.Mesh ) { node.castShadow = true; } } );
 		// console.log(hot_air);
 		scene.add(hot_air);
@@ -260,14 +267,14 @@ loader.load(
 	"models/car_pack.glb",
 	function (gltf) {
 		car_pack = gltf.scene;
-		car_pack.castShadow = true;
+		// car_pack.castShadow = true;
 		car_pack.position.x = -125;
 		car_pack.position.y = 0;
 		car_pack.position.z = 99;
 		car_pack.scale.x = 3;
 		car_pack.scale.y = 3;
 		car_pack.scale.z = 3;
-
+		car_pack.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		scene.add(car_pack);
 	}
 );
@@ -278,7 +285,7 @@ loader.load(
 	"models/bus1.glb",
 	function (gltf) {
 		bus1 = gltf.scene;
-		bus1.castShadow = true;
+		// bus1.castShadow = true;
 		bus1.position.x = -40;
 		bus1.position.y = 1;
 		bus1.position.z = 150;
@@ -286,7 +293,7 @@ loader.load(
 		bus1.scale.y = 3;
 		bus1.scale.z = 3;
 		bus1.rotation.y = 150;
-
+		bus1.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		scene.add(bus1);
 	}
 );
@@ -297,7 +304,7 @@ loader.load(
 	"models/bus2.glb",
 	function (gltf) {
 		bus2 = gltf.scene;
-		bus2.castShadow = true;
+		// bus2.castShadow = true;
 		bus2.position.x = 70;
 		bus2.position.y = 1;
 		bus2.position.z = 52;
@@ -305,7 +312,7 @@ loader.load(
 		bus2.scale.y = 0.55;
 		bus2.scale.z = 0.55;
 		bus2.rotation.y = -400;
-
+		bus2.traverse( function( node ) { if ( node.isMesh) { node.castShadow = true; } } );
 		scene.add(bus2);
 	}
 );
